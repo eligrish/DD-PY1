099fd4b4ca38f972561ab169fd580f5bce53c1dc
eligrish <115579735+eligrish@users.noreply.github.com>
2022-11-13 20:56:36 +0300

    Add files via upload

---
+++ Лабораторная 5/task1.py
@@ -0,0 +1,9 @@
+# TODO решить с помощью list comprehension и распечатать его
+from pprint import pprint
+
+def dict_convert(number: int) -> dict:
+    return {'bin': bin(number), 'dec': number, 'hex': hex(number), 'oct': oct(number)}
+
+list_dict = [dict_convert(i) for i in range(16)]
+
+pprint(list_dict)
